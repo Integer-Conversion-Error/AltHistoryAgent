4fc81a2b53730999c8a8e3a854134b88648370e9
Integer-Conversion-Error <esad.n.kaya@gmail.com>
2025-03-05 03:46:19 -0500

    adding planning comments

--- summarizers/high_level_context_distributor.py
+++ summarizers/high_level_context_distributor.py
@@ -51,10 +51,17 @@ def generate_json_object(model, json_schema, action, context):
         return None
 
 ## Find what events are due to transpire due to current global status + user input
-## Generate high-level actions based on this
+    ## Generate high-level actions based on this (e.g. adding conflict event, creating strategic interest, etc.)
+    ## Check across all schemas to see if anything needs to be updated.
 ## See about what to do with high-level actions
 ## Decided required context (?)
 ## Get all context  (or just required context?)
     ## Get JSON elements by keyword (e.g nation name, nation in region, etc.)
 ## Seperate paragraph form context to different sections of schemas. 
     ## Also seperate based on nations
+    
+    
+## Notes: 
+    ## Avoid situations where the entire JSON data is being fed into the AI and is asked to change some part and return the new part. Handle DB operations outside genAI api. (Maybe direct them using a reasoning model though.)
+    ## Learn to query objects based on some key (e.g. based on nation name).
+    ## Maybe add narration of certain items?
